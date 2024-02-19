18f5557290cb429069f2b239388a47dcf5a5217b
me <rosllarr@tutanota.com>
2024-02-19 15:49:28 +0700

    update tmux.conf

--- setup.sh
+++ setup.sh
@@ -163,7 +163,7 @@ if [ ! -f $usr_bin/kubectl ]; then
 	# install kubectl
 	curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 	chmod +x kubectl
-	sudo mv kubectl $usr_bin/
+	sudo mv kubectl $usr_bin/	
 fi
 
 if [ ! -f $usr_bin/kubectx ]; then
